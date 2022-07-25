45ce6b6191a193100bd8b0a2fdf9771df04ba523
Anex <anexbensi@gmail.com>
2022-07-25 16:16:16 +0530

    Updates

---
+++ precache-manifest.0b3df8aa5daa8833dc15fa58a556ff48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "576cc66a7e5d6783d7e3fba9dd8f4b6b",
+    "url": "/Boring/index.html"
+  },
+  {
+    "revision": "dd470598a3b957ed87b3",
+    "url": "/Boring/static/css/main.36f42c28.chunk.css"
+  },
+  {
+    "revision": "951690caa5ad9f1cde19",
+    "url": "/Boring/static/js/2.0b3b24ec.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/Boring/static/js/2.0b3b24ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd470598a3b957ed87b3",
+    "url": "/Boring/static/js/main.ac3bfa34.chunk.js"
+  },
+  {
+    "revision": "57eec9ee6d6a2e8a0925",
+    "url": "/Boring/static/js/runtime-main.9ac79a3c.js"
+  }
+]);
